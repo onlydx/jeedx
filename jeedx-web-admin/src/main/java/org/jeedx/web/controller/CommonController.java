973f47326097f938def60c62c5803a45184eaadf
Administrator <Administrator@SKY-20151109TXO>
2016-05-09 14:28:38 +0800

    控制层修改

--- jeedx-web-admin/src/main/java/org/jeedx/web/controller/CommonController.java
+++ jeedx-web-admin/src/main/java/org/jeedx/web/controller/CommonController.java
@@ -2,7 +2,14 @@ package org.jeedx.web.controller;
 
 import org.springframework.stereotype.Controller;
 
+/**
+ * 作者： dengxin
+ * 
+ * 描述：
+ *
+ * 时间： 2016年5月9日
+ */
 @Controller
-public class CommonController {
+public interface CommonController {
 
 }
